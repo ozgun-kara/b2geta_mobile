b69fa326877730a6c88926e657c57caa40e4868a
ozgun-kara <ozgunkara.devp@gmail.com>
2022-12-01 19:43:07 +0300

    Splash Page has modified

--- lib/app_constants.dart
+++ lib/app_constants.dart
@@ -1,3 +1,38 @@
+import 'package:flutter/material.dart';
+
 class AppConstants {
   String appFontFamily = "Axiforma";
+
+  Color white1 = const Color(0xFFFFFFFF);
+  Color white2 = const Color(0xFFFBFCFF);
+  Color white3 = const Color(0xFFEBEDF6);
+  Color white4 = const Color(0xFF9B9CB0);
+  Color white5 = const Color(0xFF8695AC);
+  Color white6 = const Color(0xFFC6C9CE);
+  Color white7 = const Color(0xFF9FA7BE);
+  Color white8 = const Color(0xFFD8DAE4);
+  Color white9 = const Color(0xFFAEB1D0);
+  Color white10 = const Color(0xFFADBCD2);
+  Color white11 = const Color(0xFF636484);
+  Color white12 = const Color(0xFFF4F6FC);
+  Color white13 = const Color(0xFFC4CEF1);
+  Color white14 = const Color(0xFFFAFBFD);
+  Color white15 = const Color(0xFFF3F5FB);
+  Color white16 = const Color(0xFFF1F3FF);
+
+  Color green1 = const Color(0xFF4CBD17);
+
+  Color blue1 = const Color(0xFF5F77FF);
+  Color blue2 = const Color(0xFF2943D6);
+  Color blue3 = const Color(0xFF022255);
+
+  Color black1 = const Color(0xFF000000);
+  Color black2 = const Color(0xFF022255);
+  Color black3 = const Color(0xFF2D304B);
+  Color black4 = const Color(0xFF22243B);
+  Color black5 = const Color(0xFF141526);
+  Color black6 = const Color(0xFF303745);
+  Color black7 = const Color(0xFF383A56);
+  Color black8 = const Color(0xFF404365);
+  Color black9 = const Color(0xFF2B2D38);
 }
