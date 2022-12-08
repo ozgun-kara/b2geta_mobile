5fb214d16fc9f24ab375919e26c2a28a68248680
ozgun-kara <ozgunkara.devp@gmail.com>
2022-12-08 19:40:13 +0300

    Back-End has modified

--- lib/services/login_register/login_service.dart
+++ lib/services/login_register/login_service.dart
@@ -9,25 +9,24 @@ import 'package:dio/dio.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginService {
-  final String BASE_URL = "https://api.businessucces.com/api/";
-
   void loginCall({required String email, required String password}) async {
     final response = await http.post(
-      Uri.parse('https://api.businessucces.com/api/member/login'),
+      Uri.parse(Constants.apiUrl + 'member/login'),
       headers: {
         "Content-Type": "application/x-www-form-urlencoded",
       },
-      encoding: Encoding.getByName('utf-8'),
+      // encoding: Encoding.getByName('utf-8'),
       body: {"username": email, "password": password},
     );
 
     if (response.statusCode == 200) {
-      // var result = LoginModel.fromJson(response.body);
-
       debugPrint("STATUS CODE: " + response.statusCode.toString());
       debugPrint("RESPONSE DATA: " + response.body.toString());
+
+      // var result = LoginModel.fromJson(response.body);
+
     } else {
-      debugPrint("200 LOGIN ISTEGI ATILAMADI");
+      debugPrint("DATA ERROR\nSTATUS CODE: " + response.statusCode.toString());
 
       // throw ("Bir sorun oluştu ${response.statusCode}");
     }
