cf232993d298acf27366ce4bc8fca0dcf6d7b01d
ozgun-kara <ozgunkara.devp@gmail.com>
2022-12-13 15:32:18 +0300

    Back-End has modified

--- lib/app_languages.dart
+++ lib/app_languages.dart
@@ -7,11 +7,37 @@ class AppLanguages extends Translations {
           // LANGUAGE PAGE
           'Language Selection': 'Language Selection',
           'Save': 'Save',
+          // LOGIN PAGE
+          'Member Login': 'Member Login',
+          'E-mail': 'E-mail',
+          'Password': 'Password',
+          'Remember me': 'Remember me',
+          'Forgot Password': 'I forgot my password',
+          'Login': 'Login',
+          'Sign Up': 'Sign Up',
+          'About Us': 'About Us',
+          'Privacy': 'Privacy',
+          'Terms and Conditions': 'Terms and Conditions',
+          'Contact': 'Contact',
+          'Copyright': 'Copyright 2022 B2Geta. All rights reserved',
         },
         'tr_TR': {
           // LANGUAGE PAGE
           'Language Selection': 'Dil Seçimi',
           'Save': 'Kaydet',
+          // LOGIN PAGE
+          'Member Login': 'Üye Girişi',
+          'E-mail': 'E-mail',
+          'Password': 'Şifre',
+          'Remember me': 'Beni hatırla',
+          'Forgot Password': 'Şifremi Unuttum?',
+          'Login': 'Giriş Yap',
+          'Sign Up': 'Üye Ol',
+          'About Us': 'Hakkımızda',
+          'Privacy': 'Gizlilik',
+          'Terms and Conditions': 'Kurallar ve Şartlar',
+          'Contact': 'İletişim',
+          'Copyright': 'Copright 2022 B2Geta. Tüm Hakları Saklıdır',
         },
         'de_DE': {
           // LANGUAGE PAGE
