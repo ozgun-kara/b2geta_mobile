817c83c6daa62fe6cc6aafc5eef84012457819cc
ozgun-kara <ozgunkara.devp@gmail.com>
2023-01-05 12:54:11 +0300

    Marketplace Page have modified

---
+++ lib/models/dummy_models/customer_comment_dummy_model.dart
@@ -0,0 +1,51 @@
+class CustomerCommentDummyModel {
+  int? id;
+  String? name;
+  String? rating;
+  String? date;
+  String? flagImgUrl;
+  String? country;
+  String? comment;
+  List<dynamic>? images;
+
+  CustomerCommentDummyModel(
+      {this.id,
+      this.name,
+      this.rating,
+      this.date,
+      this.flagImgUrl,
+      this.country,
+      this.comment,
+      this.images});
+
+  CustomerCommentDummyModel.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    rating = json['rating'];
+    date = json['date'];
+    flagImgUrl = json['flagImgUrl'];
+    country = json['country'];
+    comment = json['comment'];
+    if (json['images'] != null) {
+      images = <Null>[];
+      json['images'].forEach((v) {
+        images!.add(v);
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['name'] = this.name;
+    data['rating'] = this.rating;
+    data['date'] = this.date;
+    data['flagImgUrl'] = this.flagImgUrl;
+    data['country'] = this.country;
+    data['comment'] = this.comment;
+    if (this.images != null) {
+      // data['images'] = this.images!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
