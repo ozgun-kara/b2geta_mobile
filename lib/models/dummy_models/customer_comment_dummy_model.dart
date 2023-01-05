7f9707a2e55a7c17f8c9695a666a8594c83fa1b2
ozgun-kara <ozgunkara.devp@gmail.com>
2023-01-05 16:36:26 +0300

    Marketplace Page have modified

--- lib/models/dummy_models/customer_comment_dummy_model.dart
+++ lib/models/dummy_models/customer_comment_dummy_model.dart
@@ -1,51 +1,44 @@
 class CustomerCommentDummyModel {
-  int? id;
-  String? name;
-  String? rating;
-  String? date;
-  String? flagImgUrl;
-  String? country;
-  String? comment;
-  List<Null>? images;
+  CustomerCommentDummyModel({
+    required this.id,
+    required this.name,
+    required this.rating,
+    required this.date,
+    required this.flagImgUrl,
+    required this.country,
+    required this.comment,
+    required this.images,
+  });
 
-  CustomerCommentDummyModel(
-      {this.id,
-        this.name,
-        this.rating,
-        this.date,
-        this.flagImgUrl,
-        this.country,
-        this.comment,
-        this.images});
+  int id;
+  String name;
+  String rating;
+  String date;
+  String flagImgUrl;
+  String country;
+  String comment;
+  List<String> images;
 
-  CustomerCommentDummyModel.fromJson(Map<String, dynamic> json) {
-    id = json['id'];
-    name = json['name'];
-    rating = json['rating'];
-    date = json['date'];
-    flagImgUrl = json['flagImgUrl'];
-    country = json['country'];
-    comment = json['comment'];
-    if (json['images'] != null) {
-      images = <Null>[];
-      json['images'].forEach((v) {
-        // images!.add(new Null.fromJson(v));
-      });
-    }
-  }
+  factory CustomerCommentDummyModel.fromJson(Map<String, dynamic> json) =>
+      CustomerCommentDummyModel(
+        id: json["id"],
+        name: json["name"],
+        rating: json["rating"],
+        date: json["date"],
+        flagImgUrl: json["flagImgUrl"],
+        country: json["country"],
+        comment: json["comment"],
+        images: List<String>.from(json["images"].map((x) => x)),
+      );
 
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['id'] = this.id;
-    data['name'] = this.name;
-    data['rating'] = this.rating;
-    data['date'] = this.date;
-    data['flagImgUrl'] = this.flagImgUrl;
-    data['country'] = this.country;
-    data['comment'] = this.comment;
-    if (this.images != null) {
-      // data['images'] = this.images!.map((v) => v.toJson()).toList();
-    }
-    return data;
-  }
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "name": name,
+        "rating": rating,
+        "date": date,
+        "flagImgUrl": flagImgUrl,
+        "country": country,
+        "comment": comment,
+        "images": List<dynamic>.from(images.map((x) => x)),
+      };
 }
