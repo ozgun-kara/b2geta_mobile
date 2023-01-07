70627c4d31c57ead8982fe337373dadb60acacdc
Nefise Banu Tunç <nefise.tunc45@gmail.com>
2023-01-07 15:24:35 +0300

    Basket Page added

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,6 @@
 class Constants {
+  //! const olmalı çünkü constants dosyasındaki değişiklikler const olmalı adı üstünde çünkü sabitler değiştirilemez olmalı.
   static String apiUrl = "https://api.businessucces.com/api";
+  //! bu silinecek bunun kullanılan yerlerine user provider ile çekilecek.
   static String? userToken;
 }
