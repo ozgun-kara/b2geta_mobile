9a9d7e10fbdfe56b2692a59417cf00de579926a4
Nefise Banu Tunç <nefise.tunc45@gmail.com>
2023-01-20 16:33:11 +0300

    Completed : Message :+1:

---
+++ lib/models/message_model2.dart
@@ -0,0 +1,10 @@
+class MessageModel2 {
+  String text;
+  DateTime date;
+  bool isSentByMe;
+  MessageModel2({
+    required this.text,
+    required this.date,
+    required this.isSentByMe,
+  });
+}
