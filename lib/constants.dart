8b527115b4ab71a147a48a1fddb33591a397c509
Nefise Banu Tunç <nefise.tunc45@gmail.com>
2023-02-15 01:07:24 +0300

    Updated : Constant

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,5 @@
 class Constants {
-  //! const olmalı çünkü constants dosyasındaki değişiklikler const olmalı adı üstünde çünkü sabitler değiştirilemez olmalı.
+  const Constants._();
   static String apiUrl = "https://api.businessucces.com/api";
   //! bu silinecek bunun kullanılan yerlerine user provider ile çekilecek.
   static String? userToken;
