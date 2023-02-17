f816c345da9421a65acf42cf2bdde8b337ff3aad
ozgun-kara <ozgunkara.devp@gmail.com>
2023-02-17 14:17:09 +0300

    Pages have fixed

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,14 +1,14 @@
 class Constants {
   const Constants._();
-  static String apiUrl = "https://api.businessucces.com/api";
-  //! bu silinecek bunun kullanılan yerlerine user provider ile çekilecek.
+  static String apiUrl =
+      "https://api.businessucces.com/api"; // THIS VARIABLE WILL BE CHANGED WITH PROVIDER IN THE FUTURE
   static String? userToken;
 
-//!Bu buradan kaldırılacak.
+// THIS VARIABLE (headers) WILL BE REMOVED FROM HERE IN THE FUTURE
   static final Map<String, String> headers = {
     "Content-Type": "application/json; charset=utf-8",
     "Authorization": "Bearer ${Constants.userToken}",
     "Accept-Language":
-        "tr", //provider den çekilecek sabit olmamalı ve değer local db de tutulmalı.
+        "tr", // THE LANGUAGE VARIABLE MUST COME FROM THE PROVIDER AND STORED IN THE LOCAL DB
   };
 }
