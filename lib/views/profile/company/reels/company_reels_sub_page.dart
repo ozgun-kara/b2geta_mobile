08d9612bf840bf5004ec8e82767be18896da2ade
Nefise Banu Tunç <nefise.tunc45@gmail.com>
2023-02-23 10:20:21 +0300

    Updated : Reels Page

--- lib/views/profile/company/reels/company_reels_sub_page.dart
+++ lib/views/profile/company/reels/company_reels_sub_page.dart
@@ -1,4 +1,6 @@
+import 'package:b2geta_mobile/models/feed_model.dart';
 import 'package:b2geta_mobile/providers/theme_provider.dart';
+import 'package:b2geta_mobile/services/social_services/social_services.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:b2geta_mobile/views/homepage/reels_page.dart';
@@ -12,31 +14,28 @@ class CompanyReelsSubPage extends StatefulWidget {
 
 class _CompanyReelsSubPageState extends State<CompanyReelsSubPage> {
   ScrollController scrollController = ScrollController();
-
-  List reelImage = [
-    "assets/images/dummy_images/reels_image_1.png",
-    "assets/images/dummy_images/reels_image_2.png",
-    "assets/images/dummy_images/reels_image_3.png",
-    "assets/images/dummy_images/reels_image_4.png",
-    "assets/images/dummy_images/reels_image_5.png",
-    "assets/images/dummy_images/reels_image_6.png",
-    "assets/images/dummy_images/reels_image_7.png",
-    "assets/images/dummy_images/reels_image_8.png",
-    "assets/images/dummy_images/reels_image_9.png",
-    "assets/images/dummy_images/reels_image_1.png",
-    "assets/images/dummy_images/reels_image_2.png",
-    "assets/images/dummy_images/reels_image_3.png",
-    "assets/images/dummy_images/reels_image_4.png",
-    "assets/images/dummy_images/reels_image_5.png",
-    "assets/images/dummy_images/reels_image_6.png",
-    "assets/images/dummy_images/reels_image_7.png",
-    "assets/images/dummy_images/reels_image_8.png",
-  ];
+  final SocialServices _socialServices = SocialServices();
+  List<FeedModel> reelsList = [];
 
   late double deviceTopPadding;
   late double deviceWidth;
   late double deviceHeight;
   late bool themeMode;
+
+  @override
+  void initState() {
+    super.initState();
+    getReels();
+  }
+
+  void getReels() async {
+    await _socialServices.getAllReelsCall(
+      queryParameters: {"offset": "0", "limit": "25", "type": "reels"},
+    ).then((feedList) {
+      reelsList = feedList;
+      setState(() {});
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -51,7 +50,7 @@ class _CompanyReelsSubPageState extends State<CompanyReelsSubPage> {
         mainAxisSpacing: 3.0,
         crossAxisSpacing: 3.0,
       ),
-      delegate: SliverChildBuilderDelegate(childCount: reelImage.length,
+      delegate: SliverChildBuilderDelegate(childCount: reelsList.length,
           (context, index) {
         return SizedBox(
           width: 128,
@@ -62,11 +61,14 @@ class _CompanyReelsSubPageState extends State<CompanyReelsSubPage> {
                   context,
                   MaterialPageRoute(
                     builder: (context) => ReelsPage(
-                      imageUrl: reelImage[index],
+                      reelsList: reelsList,
+                      videoUrlIndex: index,
                     ),
                   ));
             },
-            child: Image.asset(reelImage[index]),
+            child: Container(
+              color: Colors.black,
+            ),
           ),
         );
       }),
