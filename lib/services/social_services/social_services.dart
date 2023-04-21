0c8aa8505babd3a16323c3f545d858c6f828aeb3
Nefise Banu Tunç <nefise.tunc45@gmail.com>
2023-04-21 10:44:06 +0300

    Update : Discover Page

--- lib/services/social_services/social_services.dart
+++ lib/services/social_services/social_services.dart
@@ -294,6 +294,48 @@ class SocialServices {
     }
   }
 
+
+  // Discover LIST
+  Future<List<FeedModel>> getDiscover(
+      {required Map<String, String> queryParameters}) async {
+    List<FeedModel> feedList = [];
+
+    try {
+      final response = await http.get(
+        Uri.parse('${Constants.apiUrl}/feeds')
+            .replace(queryParameters: queryParameters),
+        headers: {"Authorization": "Bearer ${Constants.userToken}"},
+      );
+
+      if (response.statusCode == 200) {
+        var status = json.decode(response.body)["status"];
+        if (status == true) {
+          var dataList = json.decode(response.body)["data"]["feeds"];
+          int limit = jsonDecode(response.body)["data"]["limit"];
+          int total = jsonDecode(response.body)["data"]["total"];
+          var dataListLength = total > limit ? limit : total;
+
+          for (var i = 0; i < dataListLength; i++) {
+            feedList.add(FeedModel.fromJson(dataList[i]));
+          }
+
+          return feedList;
+        } else {
+          // throw ("DATA ERROR\nSTATUS CODE:  ${response.statusCode}");
+          return feedList;
+        }
+      } else {
+        // throw ("API ERROR\nSTATUS CODE:  ${response.statusCode}");
+
+        return feedList;
+      }
+    } catch (e) {
+      debugPrint("Error !!! {$e.toString}");
+      return feedList;
+    }
+  }
+
+
   // FEED LIKE
   Future<bool> feedLikeCall({required String feedId}) async {
     final response = await http.post(
