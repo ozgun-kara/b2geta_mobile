0fbd99cb2ba1e967f2acde32eb242a234274663b
Nefise Banu Tunç <nefise.tunc45@gmail.com>
2023-05-01 13:21:08 +0300

    Update : Market Place Page

---
+++ lib/views/marketplace/models/banner_model.dart
@@ -0,0 +1,8 @@
+class BannerModel {
+  final String imageUrl;
+  final double width;
+  final double height;
+
+  BannerModel(
+      {required this.imageUrl, required this.width, required this.height});
+}
