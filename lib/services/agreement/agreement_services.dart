9e84d68045394cf2754c2cfdf859f210067420a6
Nefise Banu Tunç <nefise.tunc45@gmail.com>
2023-05-18 17:58:44 +0300

    Update : Clarification Text Sub Page

--- lib/services/agreement/agreement_services.dart
+++ lib/services/agreement/agreement_services.dart
@@ -5,40 +5,143 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class AgreementServices {
-
-  
-  Future<String> categoriesCall({Map<String, String>? queryParameters}) async {
+  //Clarification Text
+  Future<String?> clarificationTextCall() async {
     final response = await http.get(
-      Uri.parse('${Constants.apiUrl}/categories')
-          .replace(queryParameters: queryParameters),
+      Uri.parse('${Constants.apiUrl}/aggrements/clarificationtext'),
       headers: {
-        "Accept-Language": Constants.language!,
+        "Accept-Language": Constants.language ?? '',
       },
     );
 
     final responseBody = jsonDecode(utf8.decode(response.bodyBytes));
 
     if (response.statusCode == 200) {
-      debugPrint("STATUS CODE: ${response.statusCode}");
-      debugPrint("RESPONSE BODY: $responseBody");
-      // debugPrint("RESPONSE DATA: ${response.body}");
-
       var status = json.decode(response.body)["status"];
-      var dataList = json.decode(response.body)["data"];
+      var content = json.decode(response.body)["data"]["content"];
       if (status == true) {
-        return '';
+        return content;
       } else {
         debugPrint("DATA ERROR\nSTATUS CODE: ${response.statusCode}");
         debugPrint("responseCode: ${responseBody["responseCode"]}");
         debugPrint("responseText: ${responseBody["responseText"]}");
-        // throw ("DATA ERROR\nSTATUS CODE:  ${response.statusCode}");
-        return '';
+        return null;
       }
     } else {
       debugPrint("API ERROR\nSTATUS CODE: ${response.statusCode}");
-      // throw ("API ERROR\nSTATUS CODE:  ${response.statusCode}");
+      return null;
+    }
+  }
 
-      return '';
+  //Distance Sales Agreement
+  Future<String?> distanceSalesAgreementCall() async {
+    final response = await http.get(
+      Uri.parse('${Constants.apiUrl}/aggrements/distancesalesagreement'),
+      headers: {
+        "Accept-Language": Constants.language ?? '',
+      },
+    );
+
+    final responseBody = jsonDecode(utf8.decode(response.bodyBytes));
+
+    if (response.statusCode == 200) {
+      var status = json.decode(response.body)["status"];
+      var content = json.decode(response.body)["data"]["content"];
+      if (status == true) {
+        return content;
+      } else {
+        debugPrint("DATA ERROR\nSTATUS CODE: ${response.statusCode}");
+        debugPrint("responseCode: ${responseBody["responseCode"]}");
+        debugPrint("responseText: ${responseBody["responseText"]}");
+        return null;
+      }
+    } else {
+      debugPrint("API ERROR\nSTATUS CODE: ${response.statusCode}");
+      return null;
+    }
+  }
+
+  //Prior Information Distance Sales
+  Future<String?> priorInformationDistanceSalesAgreementCall() async {
+    final response = await http.get(
+      Uri.parse('${Constants.apiUrl}/aggrements/priorinformationdistancesales'),
+      headers: {
+        "Accept-Language": Constants.language ?? '',
+      },
+    );
+
+    final responseBody = jsonDecode(utf8.decode(response.bodyBytes));
+
+    if (response.statusCode == 200) {
+      var status = json.decode(response.body)["status"];
+      var content = json.decode(response.body)["data"]["content"];
+      if (status == true) {
+        return content;
+      } else {
+        debugPrint("DATA ERROR\nSTATUS CODE: ${response.statusCode}");
+        debugPrint("responseCode: ${responseBody["responseCode"]}");
+        debugPrint("responseText: ${responseBody["responseText"]}");
+        return null;
+      }
+    } else {
+      debugPrint("API ERROR\nSTATUS CODE: ${response.statusCode}");
+      return null;
+    }
+  }
+
+  //Privacy Policy
+  Future<String?> privacyPolicyCall() async {
+    final response = await http.get(
+      Uri.parse('${Constants.apiUrl}/aggrements/privacy'),
+      headers: {
+        "Accept-Language": Constants.language ?? '',
+      },
+    );
+
+    final responseBody = jsonDecode(utf8.decode(response.bodyBytes));
+
+    if (response.statusCode == 200) {
+      var status = json.decode(response.body)["status"];
+      var content = json.decode(response.body)["data"]["content"];
+      if (status == true) {
+        return content;
+      } else {
+        debugPrint("DATA ERROR\nSTATUS CODE: ${response.statusCode}");
+        debugPrint("responseCode: ${responseBody["responseCode"]}");
+        debugPrint("responseText: ${responseBody["responseText"]}");
+        return null;
+      }
+    } else {
+      debugPrint("API ERROR\nSTATUS CODE: ${response.statusCode}");
+      return null;
+    }
+  }
+
+  //Delivery and Return
+  Future<String?> deliveryAndReturnPolicyCall() async {
+    final response = await http.get(
+      Uri.parse('${Constants.apiUrl}/aggrements/delivery'),
+      headers: {
+        "Accept-Language": Constants.language ?? '',
+      },
+    );
+
+    final responseBody = jsonDecode(utf8.decode(response.bodyBytes));
+
+    if (response.statusCode == 200) {
+      var status = json.decode(response.body)["status"];
+      var content = json.decode(response.body)["data"]["content"];
+      if (status == true) {
+        return content;
+      } else {
+        debugPrint("DATA ERROR\nSTATUS CODE: ${response.statusCode}");
+        debugPrint("responseCode: ${responseBody["responseCode"]}");
+        debugPrint("responseText: ${responseBody["responseText"]}");
+        return null;
+      }
+    } else {
+      debugPrint("API ERROR\nSTATUS CODE: ${response.statusCode}");
+      return null;
     }
   }
 }
