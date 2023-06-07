bc85d7b2d376e46c04fc8f4b7230f2088aacb14b
Nefise Banu Tunç <nefise.tunc45@gmail.com>
2023-06-07 19:07:43 +0300

    Update : Account Settings Sub Page

--- lib/views/menu/sub_pages/account_settings/account_settings_sub_page.dart
+++ lib/views/menu/sub_pages/account_settings/account_settings_sub_page.dart
@@ -200,7 +200,7 @@ class _AccountSettingsSubPageState extends State<AccountSettingsSubPage> {
                                   BorderRadius.all(Radius.circular(16)),
                             ),
                             child: Text(
-                              'Close'.tr,
+                              'No'.tr,
                               style: TextStyle(
                                   fontSize: 14,
                                   fontFamily: AppTheme.appFontFamily,
