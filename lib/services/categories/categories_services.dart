cc28e096994fadd2e455428212bc1015e6a939ea
ozgun-kara <ozgunkara.devp@gmail.com>
2023-07-17 12:27:25 +0300

    Update categories_services.dart

--- lib/services/categories/categories_services.dart
+++ lib/services/categories/categories_services.dart
@@ -1,8 +1,4 @@
-import 'dart:convert';
-import 'package:b2geta_mobile/constants.dart';
-import 'package:b2geta_mobile/models/categories/category_model.dart';
-import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+
 
 class CategoriesServices {
   // CATEGORIES
