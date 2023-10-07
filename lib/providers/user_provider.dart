2c88d21fb801538d7772b6c2f0f1a3b2ec36769c
Nefise Banu Tunç <100851004+NefiseTnc@users.noreply.github.com>
2023-10-07 16:18:37 +0300

    Update user_provider.dart

--- lib/providers/user_provider.dart
+++ lib/providers/user_provider.dart
@@ -6,7 +6,6 @@ import 'package:b2geta_mobile/models/user/user_model.dart';
 import 'package:b2geta_mobile/services/general_service.dart';
 import 'package:b2geta_mobile/services/member/member_services.dart';
 import 'package:flutter/material.dart';
-import 'package:get_storage/get_storage.dart';
 
 class UserProvider with ChangeNotifier {
   UserModel _user = UserModel();
