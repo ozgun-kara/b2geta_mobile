765dc1c3fe077fc9c27eae2308cf854edfd8ecd4
ozgun-kara <ozgunkara.devp@gmail.com>
2023-12-03 00:45:52 +0300

    Update

--- lib/views/marketplace/models/banner_model.dart
+++
@@ -1,8 +0,0 @@
-class SliderModel {
-  final String imageUrl;
-  final double width;
-  final double height;
-
-  SliderModel(
-      {required this.imageUrl, required this.width, required this.height});
-}
